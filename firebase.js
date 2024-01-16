97c1dff189ecb3f6628ffb9068857e64467ccdfb
mproyyan <mproyyan@gmail.com>
2024-01-16 09:18:55 +0700

    data model practices

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
+import { getFirestore, doc, getDoc, collection, getDocs } from "firebase/firestore"
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FB_API_KEY,
@@ -12,5 +12,38 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
+
+const getDocumentReference = async () => {
+    try {
+        const alovelaceRef = doc(db, "users/alovelace")
+        const alovelace = await getDoc(alovelaceRef)
+
+        console.log(alovelaceRef)
+        console.log(alovelace.data())
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getDocumentReference()
+
+const getCollectionReference = async () => {
+    try {
+        const usersCollectionRef = collection(db, "users")
+        const querySnapshot = await getDocs(usersCollectionRef)
+
+        const userReviewSubcollection = collection(db, "users/alovelace/reviews")
+        const reviews = await getDocs(user)
+
+        console.log(usersCollectionRef)
+        querySnapshot.forEach(user => {
+            console.log(user.data())
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getCollectionReference()
 
 export default db
