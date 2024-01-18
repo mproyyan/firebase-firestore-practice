980b0f3ba535975589d4f734fabb5401429c822d
mproyyan <mproyyan@gmail.com>
2024-01-18 10:04:53 +0700

    pagination

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
+import { collection, getDocs, getFirestore, limit, orderBy, query, startAfter } from "firebase/firestore"
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FB_API_KEY,
@@ -12,5 +12,35 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
+
+const paginate = async (cursor) => {
+    const citiesRef = collection(db, "cities")
+    let q = query(citiesRef, orderBy("population"), limit(2))
+    if (typeof cursor !== "undefined") {
+        console.log("page changed")
+        q = query(citiesRef, orderBy("population"), startAfter(cursor), limit(2))
+    }
+
+    const cities = await getDocs(q)
+    cities.forEach(city => {
+        console.log(city.data())
+    });
+
+    // DocumentSnapshot can be query cursor
+    const nextCursor = cities.docs[cities.docs.length - 1]
+    console.log(nextCursor)
+    return nextCursor
+}
+
+const runPagination = async () => {
+    try {
+        const cursor = await paginate()
+        paginate(cursor)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+runPagination()
 
 export default db
